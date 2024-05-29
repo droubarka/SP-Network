cd0549f8b70ccdba2b5b3fafcaeeb270fb6bbe40
droubarka <droubarka@github.com>
2024-05-28 21:24:11 -0400

    Added new function schedule_key to main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def schedule_key(key: int, rounds: int) -> list[int]:
+	"""
+	This function takes a key and the number of rounds as arguments
+	and returns a list of scheduled keys using a simple equation.
+	"""
+
+	scheduled_keys = [key]
+
+	for _ in range(1, rounds):
+		scheduled_keys.append(
+			scheduled_keys[-1] ^ (scheduled_keys[-1] >> 3)
+		)
+
+	return scheduled_keys
