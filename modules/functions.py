83056948c5eeb8c9ab5b90e2770a2f99596badc5
droubarka <droubarka@github.com>
2024-05-28 22:06:44 -0400

    Add new file: /modules/functions.py

---
+++ modules/functions.py
@@ -0,0 +1,23 @@
+# /modules/functions.py
+
+def schedule_key(key: int, rounds: int) -> list[int]:
+	pass
+
+def substitute(block: int) -> int:
+	pass
+
+def permute(block: int) -> int:
+	pass
+
+def encrypt_block(block: int, key: int) -> int:
+	pass
+
+def decrypt_block(block: int, key: int) -> int:
+	pass
+
+def encrypt_file(input_file: str, output_file: str, key: int) -> int:
+	pass
+
+def decrypt_file(input_file: str, output_file: str, key: int) -> int:
+	pass
+
