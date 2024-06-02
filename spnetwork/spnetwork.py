b96097081f074042296a431225eef94a6fa4ffd3
droubarka <droubarka@gmail.com>
2024-06-02 11:57:57 -0400

    Update file: /spnetwork/spnetwork.py

--- spnetwork/spnetwork.py
+++ spnetwork/spnetwork.py
@@ -20,8 +20,8 @@ def substitute(block: bytes, table: bytes, reverse: bool = False) -> bytes:
 	if not isinstance(table, bytes):
 		raise ValueError("table argument must be a bytes object")
 
-	if len(set(table)) != 256: #?
-		raise ValueError("table argument must contain exactly 256 unique elements")
+	if len(set(table)) != len(table):
+		raise ValueError("Each byte in the block must have a unique substitution in the table")
 
 	if reverse:
 		substituted_block = bytes(table.index(byte) for byte in block)
@@ -31,8 +31,17 @@ def substitute(block: bytes, table: bytes, reverse: bool = False) -> bytes:
 	return substituted_block
 
 
-def permute(block: bytes, table: bytes, reverse: bool = False) -> bytes:
+def permute_bits(block: bytes, table: bytes, reverse: bool = False) -> bytes:
 	"""
+	Perform bit permutation on a block of bytes in either direction.
+
+	Args:
+		block: A block of bytes to be permuted.
+		table: A bytes object representing the permutation table.
+		reverse: A boolean flag to determine the direction of permutation.
+
+	Returns:
+		A block of bytes resulting from the permutation.
 	"""
 	if not isinstance(block, bytes):
 		raise ValueError("block argument must be a bytes object")
@@ -40,19 +49,17 @@ def permute(block: bytes, table: bytes, reverse: bool = False) -> bytes:
 	if not isinstance(table, bytes):
 		raise ValueError("table argument must be a bytes object")
 
-	if 256 < len(set(table)): #?
-		raise ValueError("")
-
-	#if 32 < len(block):
-		#raise ValueError("")
+	if len(set(table)) != len(table):
+		raise ValueError("Each bit in the block must have a unique permutation in the table")
 
 	if len(table) // 8 != len(block):
-		raise ValueError("")
+		raise ValueError("The table must be able to accommodate all bits in the block")
 
 	block_bits = functions.bytes_to_bits(block)
 
 	if reverse:
-		permuted_block_bits = ''.join(block_bits[table.index(bit)] for bit in range(len(block_bits)))
+		permuted_block_bits = ''.join( \
+			block_bits[table.index(bit)] for bit in range(len(block_bits)))
 	else:
 		permuted_block_bits = ''.join(block_bits[idx] for idx in table)
 
