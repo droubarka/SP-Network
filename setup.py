6ae5593b56a38f6c4e542b5d31b5ffc37c7fb6c0
Mohamed A. Oubarka <droubarka@gmail.com>
2024-06-07 05:06:57 -0400

    Update README.md

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 
-with open("README.md", 'r') as file:
-	long_description = file.read()
-
 setup(
-	name="spnetwork",
-	version='0.0.10',
-	description="SP-Network",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	author="Mohamed A. Oubarka",
-	author_email="droubarka@gmail.com",
-	url="https://github.com/droubarka/sp-network",
-	license="",
+	name='spnetwork',
+	version='1.0.0',
 	packages=find_packages(),
-	python_requires=">=3.8",
+	author='Mohamed A. Oubarka',
+	author_email='droubarka@gmail.com',
+	url='https://github.com/droubarka/sp-network.git',
+	description='', #?
+	long_description=open('README.md').read(),
+	long_description_content_type="text/markdown",
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License ::  :: ', #?
+	],
+	keywords=(), #?
+	license='', #?
+	install_requires=[],
+	zip_safe=False
 )
